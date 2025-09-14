5331ae1f18f15a64a44f85677bad5424099e18a4
Artur Akopov <106455653+aarturio@users.noreply.github.com>
2025-09-13 20:50:59 -0400

    feat: Add new tables for time periods

--- backend/core/market-data-client.js
+++ backend/core/market-data-client.js
@@ -1,14 +1,14 @@
-import stocks from "../config/stocks.json" with { type: "json" };
+import { TICKERS } from "../config.js";
 
 class MarketDataClient {
   constructor() {
     this.keyId = process.env.ALPACA_API_KEY;
     this.secretKey = process.env.ALPACA_SECRET_KEY;
     this.feed = "iex";
-    this.symbols = stocks.join("%2C");
+    this.symbols = TICKERS.join("%2C");
   }
 
-  async getBars(start, end, page_token = null, timeframe = "1H") {
+  async getBars(start, end, timeframe, page_token = null) {
     let baseUrl = `https://data.alpaca.markets/v2/stocks/bars?symbols=${this.symbols}&timeframe=${timeframe}&start=${start}&end=${end}&limit=10000&adjustment=raw&feed=${this.feed}&sort=asc`;
 
     let url = page_token ? baseUrl + `&page_token=${page_token}` : baseUrl;
